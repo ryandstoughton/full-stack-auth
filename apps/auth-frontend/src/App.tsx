faf9a0a0f7680fb80ce23aea317d564fb6ca852a
Ryan Stoughton <ryandstoughton@gmail.com>
2022-02-25 13:25:38 -0600

    Publish

--- apps/auth-frontend/src/App.tsx
+++ apps/auth-frontend/src/App.tsx
@@ -1,9 +1,5 @@
 
 export default function App() {
-  const login: LoginPayload = {
-    username: '',
-    password: ''
-  }
 
   return (
     <div></div>
--- libraries/interfaces/index.ts
+++ libraries/interfaces/index.ts
@@ -2,3 +2,4 @@ export type LoginPayload = {
   username: string;
   password: string;
 };
+
